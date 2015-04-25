1c44fe27f9ba7afb1751dc3089d08e749492c78f
Eugenio Cuevas Pozuelo <eucuepo@gmail.com>
2015-04-25 03:38:06 +0200

    fix twitter feed

--- js/controllers.js
+++ js/controllers.js
@@ -12,24 +12,7 @@ angular.module('myApp')
       $translate.use(key);
     };
 })
-.controller('CarouselDemoCtrl', function ($scope,$timeout) {
-
-  $scope.myInterval = 5000;
-  var slides = $scope.slides = [];
-
-  $scope.addSlide = function() {
-    var newWidth = 400 + slides.length + 1;
-    slides.push({
-      image: 'images/slide-0'+(i+1)+'.jpg',
-      text: ['Con la gente','Transparentes','Otra idea'][slides.length % 3] + ' ' +
-        ['Estamos con la gente del pueblo', 'Transparencia total', 'Otra explicación de la idea'][slides.length % 3]
-    });
-  }
-
-  for (var i=0; i<3 ;i++) {
-    $scope.addSlide();
-  }
-
+.controller('WelcomeController', function ($scope,$timeout) {
   // hack to reload twitter feed
   $timeout = twttr.widgets.load();
 });
